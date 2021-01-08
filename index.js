27bb5f1835002bac93019fbfb64eed8554fd1f28
John Dust <d24dust@hotmail.com>
2021-01-08 18:21:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Code your solutions in this file
-function writeCards() {
-  
+function writeCards(names) {
+  const cards = [];
+  for (let i=0; i < names.length; i++) {
+    cards.push(`Thank you and your face ${names[i]}`)
+  }
 }
